e62448b1754d171cce38049e190bf7fc25fa0469
Varun <varun.v2021@gmail.com>
2018-11-15 10:30:20 +0530

    Add files via upload

---
+++ SpringBootSample-microservice-forexservice1/src/main/java/com/example/demo/ExchangeValueRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+	ExchangeValue findByFromAndTo(String from, String to);
+}
